b3eb558906b969e99fb9db21e5cdbd6adeff0a6f
amal <amaljw2002@gmail.com>
2025-05-11 00:43:28 +0200

    modified:   controllers/transactionController.js

--- controllers/transactionController.js
+++ controllers/transactionController.js
@@ -1,7 +1,7 @@
 exports.analyzeTransaction = async (req, res) => {
   const { txHash } = req.body;
-  const { getTransactionDetails } = require('../services/etherscanservice');
-  const { interpretWithSwissTaxLaw } = require('../utils/swissTaxRules');
+  const { getTransactionDetails } = require('../services/etherscanService.js');
+  const { interpretWithSwissTaxLaw } = require('../utils/swissTaxRules.js');
 
 
   if (!txHash) return res.status(400).json({ error: 'Transaction hash required' });
