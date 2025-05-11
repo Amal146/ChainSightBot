11e614c4976172c330cbc34407cc0b8265af3da0
amal <amaljw2002@gmail.com>
2025-05-11 21:06:04 +0200

    modified:   server.js

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+console.log("Gemini API Key:", process.env.GEMINI_API_KEY); 
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
